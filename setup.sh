f173b5ae61fe790da253f527723a77b23b3821f2
Olivier Steck <osteck@gmail.com>
2019-03-27 15:17:22 -0400

    centralized setup & config

--- setup.sh
+++ setup.sh
@@ -1,20 +1,3 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-../base_config.sh ${DIR}/configurate.py
-
-#ROOT=tomi
-#COMPONENT=config
-#PROJECT=${ROOT}_${COMPONENT}
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#
-#rm -rf ${DIR}/build
-#
-#source activate "vanilla"
-#${DIR}/configurate.sh
-#echo "---------------------------------------------------------------------------------------------------------------------------------------"
-#
-#source activate "vanilla"
-#${DIR}/configurate.sh
-#echo "---------------------------------------------------------------------------------------------------------------------------------------"
+../tomi_tools/base_setup.sh `basename "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"`
