415d491636faf402f748f7922a59b99c70209fb9
Olivier Steck <osteck@gmail.com>
2019-03-27 15:19:50 -0400

    centralized setup & config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-../tomi_tools/base_setup.sh `basename "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+bash ../tomi_tools//base_config.sh ${DIR}/configurate.py
